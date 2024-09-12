bcf9f7cc747a4ceb65d88892cc90cdc8428ce6b4
Bandile Hlatshwayo <1851054@students.wits.ac.za>
2024-09-12 20:25:46 +0200

    Maze Class member functions implemented

--- game-source-code/Maze.cpp
+++ game-source-code/Maze.cpp
@@ -6,6 +6,39 @@ Maze::Maze(int width, int height) : tileSize_(20), pacmanStartPos_(0, 0), width_
     wallTexture_ = raylib::Texture2D("../resources/brick.png");
 }
 
-void Maze::loadMaze(const std::string& filename) { }
+void Maze::loadMaze(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line)) {
+        grid_.push_back(line);
+    }
 
-void Maze::draw() {}
+    for (int y = 0; y < grid_.size(); ++y) {
+        for (int x = 0; x < grid_[y].size(); ++x) {
+            if (grid_[y][x] == 'P') {
+                pacmanStartPos_ = raylib::Vector2(x * tileSize_, y * tileSize_);
+                grid_[y][x] = '.'; // Replace 'P' with a dot
+            }
+        }
+    }
+}
+
+void Maze::draw() {
+    for (int y = 0; y < grid_.size(); ++y) {
+        for (int x = 0; x < grid_[y].size(); ++x) {
+            raylib::Rectangle destRec{static_cast<float>(x * tileSize_), static_cast<float>(y * tileSize_), static_cast<float>(tileSize_), static_cast<float>(tileSize_)};
+            switch (grid_[y][x]) {
+                case '#':
+                    // Draw wall (blue)
+                    DrawRectangleRec(destRec, BLUE);
+                    break;
+                case '.':
+                    // We will draw
+                    break;
+                case 'o':
+                    //we will Draw power pellet   
+                    break;
+            }
+        }
+    }
+}
